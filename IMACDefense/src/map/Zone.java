06bf629a147cf624ae92dda4e7070d903617d0b1
Thais Guigon <thaisguigon@yahoo.fr>
2013-05-12 23:56:19 +0200

    Lecture Fichiers XML
    Classes XMLParser, Zone, Tile

---
+++ IMACDefense/src/map/Zone.java
@@ -0,0 +1,26 @@
+package map;
+
+public class Zone {
+	
+	private int id = 0;
+	// private Player owner = null;
+
+	public Zone(int id/*, Player owner*/) {
+		// TODO Auto-generated constructor stub
+		super ();
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
