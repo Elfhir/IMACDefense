c031b00099692424c430d9a6c961e41006f03459
Nadia Mazouz <summerangel857@gmail.com>
2013-05-15 22:45:28 +0200

    Affichage Fenêtre

---
+++ IMACDefense/src/window/GraphicalInterface.java
@@ -0,0 +1,27 @@
+package window;
+
+import javax.swing.JFrame;
+
+public class GraphicalInterface extends JFrame
+{
+ 
+
+	public GraphicalInterface(){
+		super();
+ 
+		
+		build();//On initialise notre fenêtre
+	}
+ 
+	private void build(){
+		setTitle("IMACDefense"); //On donne un titre à l'application
+		setSize(600,400); //On donne une taille à notre fenêtre
+		setLocationRelativeTo(null); //On centre la fenêtre sur l'écran
+		setResizable(false); //On interdit la redimensionnement de la fenêtre
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //On dit à l'application de se fermer lors du clic sur la croix
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+
