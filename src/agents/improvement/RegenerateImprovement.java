3db130a4c55c999fc013333a7cbe54d06174be65
Nadia Mazouz <summerangel857@gmail.com>
2013-06-06 23:12:59 +0200

    Mods Agents

---
+++ src/agents/improvement/RegenerateImprovement.java
@@ -0,0 +1,23 @@
+package agents.improvement;
+
+public class RegenerateImprovement implements ImproverInterface {
+
+	public RegenerateImprovement() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void improve() {
+		// TODO Auto-generated method stub
+		System.out.println("Increase LifePoints");
+	}
+
+}
